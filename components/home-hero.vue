bcd6974130ec3b214847465d24ab86681608d3c9
Yasmin Gonçalves <yasmin.goncalves97@hotmail.com>
2026-06-30 16:44:24 -0300

    Add CLI and track

---
+++ components/home-hero.vue
@@ -0,0 +1,9 @@
+<script setup>
+const {data} = await useContent('home-hero@1');
+</script>
+
+<template>
+  <ul v-if="data.content">
+    <li><strong>Heading:</strong> {{ data.content.heading }}</li>
+  </ul>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,24 @@
 <script setup lang="ts">
-const {data} = useContent('home-hero');
+const croct = useCroct();
+
+const track = () =>
+  croct.track("goalCompleted", {
+    goalId: "signup",
+  });
+
+const { data } = useContent("home-hero", {
+  fallback: {
+    heading: "Meu fallback",
+  },
+});
 </script>
 
 <template>
-    <main style="font-family: sans-serif; max-width: 40rem; margin: 3rem auto">
-        <h1>Croct multi-tenant</h1>
-        <pre>{{ data?.content }}</pre>
-    </main>
+  <main style="font-family: sans-serif; max-width: 40rem; margin: 3rem auto">
+    <h1>Croct multi-tenant</h1>
+    <ul v-if="data.content">
+      <li><strong>Heading:</strong> {{ data.content.heading }}</li>
+    </ul>
+    <button @click="track">Trigger</button>
+  </main>
 </template>
